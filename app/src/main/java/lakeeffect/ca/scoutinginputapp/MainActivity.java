94552c78daa44b238ea0d60c75b3f4e93dda8253
ramachandra <ajay@ajayinkingston.com>
2016-10-21 09:47:43 -0400

    did stuff

--- app/src/main/java/lakeeffect/ca/scoutinginputapp/MainActivity.java
+++ app/src/main/java/lakeeffect/ca/scoutinginputapp/MainActivity.java
@@ -4,9 +4,7 @@ import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
-import android.content.Context;
 import android.content.DialogInterface;
-import android.content.DialogInterface.OnMultiChoiceClickListener;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
@@ -14,22 +12,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Set;
 import java.util.UUID;
-
-import lakeeffect.ca.scoutinginputapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
     OutputStream out;
     InputStream in;
 
+    boolean searchopen;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,6 +49,37 @@ public class MainActivity extends AppCompatActivity {
         final BluetoothAdapter ba = BluetoothAdapter.getDefaultAdapter();
         Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
         startActivityForResult(turnOn, 0);
+
+        Button check = ((Button) findViewById(R.id.search));
+        final EditText robotNum = ((EditText) findViewById(R.id.robotnum));
+        check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File sdcard = Environment.getExternalStorageDirectory();
+                File file = new File(sdcard,"ScoutingData/" + robotNum +  ".txt");
+                StringBuilder text = new StringBuilder();
+
+                try {
+                    BufferedReader br = new BufferedReader(new FileReader(file));
+                    String line;
+
+                    int i = 0;
+
+                    while ((line = br.readLine()) != null) {
+                        if(i==0 ){
+                            i+=1;
+                            continue;
+                        }
+                        text.append(line);
+                        text.append('\n');
+                    }
+                    br.close();
+                }catch (IOException e) {
+                    e.printStackTrace();
+                }
+                String[] data = text.toString().split("end");
+            }
+        });
 
         Button connect = ((Button) findViewById(R.id.connect));
         assert(connect != null);
@@ -101,7 +133,7 @@ public class MainActivity extends AppCompatActivity {
                                                     else continue;
                                                     String message = new String(bytes, Charset.forName("UTF-8"));
                                                     if (bytes.length > 0){
-                                                        parse(data + message);
+                                                        save(data + message);
                                                         out.write("done".getBytes(Charset.forName("UTF-8")));
                                                         data = "";
                                                     }else{
@@ -157,7 +189,69 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void parse(String data){
+    public String parse(String string){
+        String[] data = string.split("\n")[1].split(","); //auto
+        String message = "Available: ";
+        ArrayList<Integer> available = new ArrayList<>();
+
+        for(int i=0;i<data.length;i+=3){
+            if(Boolean.parseBoolean(data[i])){
+                message += getDefense(i/3) + ", ";
+                available.add(new Integer(i/3));
+            }
+        }
+
+        message+= "\nReached: ";
+        for(int i=1;i<data.length;i+=3){
+            if(Boolean.parseBoolean(data[i])){
+                message += getDefense((i-1)/3) + ", ";
+            }
+        }
+
+        message+= "\nCrossed: ";
+        for(int i=2;i<data.length;i+=3){
+            if(Boolean.parseBoolean(data[i])){
+                message += getDefense((i-2)/3) + ", ";
+            }
+        }
+
+        data = string.split("\n")[2].split(","); //teleop
+
+        for(Integer i: available){
+            message += "\n" + getDefense(i) + ": " + data[i*2] + " attemps and " + data[i*2+1] + " crossed";
+        }
+
+        message += "Low Goals: " + data[data.length-4] + " attemps and " + data[data.length-3] + " goals scored";
+        message += "High Goals: " + data[data.length-2] + " attemps and " + data[data.length-1] + " goals scored";
+
+            return message;
+        }
+
+    public String getDefense(int i){
+        switch(i){
+            case 0:
+                return "Low Bar";
+            case 1:
+                return "Portcullis";
+            case 2:
+                return "Cheval de Frise";
+            case 3:
+                return "Moat";
+            case 4:
+                return "Rampart";
+            case 5:
+                return "Drawbridge";
+            case 6:
+                return "Sally Port";
+            case 7:
+                return "Rock Wall";
+            case 8:
+                return "Rough Terrain";
+        }
+        return "";
+    }
+
+    public void save(String data){
 
         File sdCard = Environment.getExternalStorageDirectory();
 
