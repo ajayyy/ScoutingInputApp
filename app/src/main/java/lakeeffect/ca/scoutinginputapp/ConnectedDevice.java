5bef67870d27d1ae05e0b690ebcf59cf0d31df7c
thebestusernameever <ajay@ajayinkingston.com>
2016-10-20 22:11:39 -0400

    Connect to multiple devices kindof, production ready maybe...

---
+++ app/src/main/java/lakeeffect/ca/scoutinginputapp/ConnectedDevice.java
@@ -0,0 +1,12 @@
+package lakeeffect.ca.scoutinginputapp;
+
+
+/**
+ * Created by Ajay on 10/20/2016.
+ */
+public class ConnectedDevice {
+
+    public ConnectedDevice(){
+
+    }
+}
--- app/src/main/java/lakeeffect/ca/scoutinginputapp/MainActivity.java
+++ app/src/main/java/lakeeffect/ca/scoutinginputapp/MainActivity.java
@@ -12,6 +12,8 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -35,104 +37,123 @@ public class MainActivity extends AppCompatActivity {
     int connectedDevices = 1;
     Thread thread;
 
+    BluetoothSocket bluetoothsocket;
+    OutputStream out;
+    InputStream in;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        parse("2708,2,0,0,0,0,0,0,1,0,0,1,1,1,0,HELLO", this);
-
-        //TODO MAKE THIS SUPPORT MULTIPLE DEVICES PLUS INFO VIEW
-
         final BluetoothAdapter ba = BluetoothAdapter.getDefaultAdapter();
         Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
         startActivityForResult(turnOn, 0);
 
-        Set<BluetoothDevice> pairedDevices = ba.getBondedDevices();
-        final BluetoothDevice[] devices = pairedDevices.toArray(new BluetoothDevice[0]);
-        String[] names = new String[devices.length];
-        if (pairedDevices.size() > 0) {
-            for (int i=0;i<devices.length;i++) {
-                names[i] = devices[i].getName();
-            }
-            new AlertDialog.Builder(this)
-                    .setTitle("Which device should I connect to?")
-                    .setMultiChoiceItems(names, null, new OnMultiChoiceClickListener(){
-                        @Override
-                        public void onClick(DialogInterface dialog, final int which, boolean isChecked) {
-                            dialog.dismiss();
-                            thread = new Thread(){
-                                public void run(){
-                                    OutputStream out = null;
-                                    InputStream in = null;
-                                    BluetoothSocket bluetoothsocket = null;
-                                    try {
-                                        bluetoothsocket = devices[which].createRfcommSocketToServiceRecord(UUID.fromString("6ba6afdc-6a0a-4b1d-a2bf-f71ac108b636"));
-                                        bluetoothsocket.connect();
-                                        out = bluetoothsocket.getOutputStream();
-                                        in = bluetoothsocket.getInputStream();
-                                        //CONNECTED!!
-                                        runOnUiThread(new Runnable() {
-                                            @Override
-                                            public void run() {
-                                                Toast.makeText(MainActivity.this, "connected!",
-                                                        Toast.LENGTH_LONG).show();
+        Button connect = ((Button) findViewById(R.id.connect));
+        assert(connect != null);
+        connect.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Set<BluetoothDevice> pairedDevices = ba.getBondedDevices();
+                final BluetoothDevice[] devices = pairedDevices.toArray(new BluetoothDevice[0]);
+                String[] names = new String[devices.length];
+                if (pairedDevices.size() > 0) {
+                    for (int i=0;i<devices.length;i++) {
+                        names[i] = devices[i].getName();
+                    }
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle("Which device should I connect to?")
+                            .setMultiChoiceItems(names, null, new DialogInterface.OnMultiChoiceClickListener(){
+                                @Override
+                                public void onClick(DialogInterface dialog, final int which, boolean isChecked) {
+                                    dialog.dismiss();
+                                    thread = new Thread(){
+                                        public void run(){
+                                            BluetoothSocket bluetoothsocket = null;
+                                            OutputStream out = null;
+                                            InputStream in = null;
+                                            try {
+
+                                                bluetoothsocket = devices[which].createRfcommSocketToServiceRecord(UUID.fromString("6ba6afdc-6a0a-4b1d-a2bf-f71ac108b636"));
+                                                bluetoothsocket.connect();
+                                                out = bluetoothsocket.getOutputStream();
+                                                in = bluetoothsocket.getInputStream();
+                                                //CONNECTED!!
+                                                runOnUiThread(new Runnable() {
+                                                    @Override
+                                                    public void run() {
+                                                        Toast.makeText(MainActivity.this, "connected!",
+                                                                Toast.LENGTH_LONG).show();
+                                                    }
+                                                });
+                                            } catch (IOException e) {
+                                                e.printStackTrace();
                                             }
-                                        });
-                                    } catch (IOException e) {
-                                        e.printStackTrace();
-                                    }
-                                    String data = "";
-                                    Log.d("HELLO", "KJLDJJLADS");
-                                    while(out != null && in != null && bluetoothsocket.isConnected()){
-                                        Log.d("HELLO", "");
-                                        try {
+                                            String data = "";
+                                            Log.d("HELLO", "KJLDJJLADS");
+                                            while(out != null && in != null && bluetoothsocket.isConnected()){
+                                                Log.d("HELLO", "");
+                                                try {
 
-                                            byte[] bytes = new byte[1000000];
-                                            int amount = in.read(bytes);
-                                            if(amount>0)  bytes = Arrays.copyOfRange(bytes, 0, amount);//puts data into bytes and cuts bytes
-                                            else continue;
-                                            String message = new String(bytes, Charset.forName("UTF-8"));
-                                            if (bytes.length > 0){
-                                                parse(data + message);
-                                                out.write("done".getBytes(Charset.forName("UTF-8")));
-                                                data = "";
-                                            }else{
-                                                data += message;
+                                                    byte[] bytes = new byte[1000000];
+                                                    int amount = in.read(bytes);
+                                                    if(amount>0)  bytes = Arrays.copyOfRange(bytes, 0, amount);//puts data into bytes and cuts bytes
+                                                    else continue;
+                                                    String message = new String(bytes, Charset.forName("UTF-8"));
+                                                    if (bytes.length > 0){
+                                                        parse(data + message);
+                                                        out.write("done".getBytes(Charset.forName("UTF-8")));
+                                                        data = "";
+                                                    }else{
+                                                        data += message;
+                                                    }
+                                                    final byte[] bytes2 = bytes;
+                                                    Log.d("HELLO", "KJLDJJLADS" + bytes);
+
+                                                } catch (IOException e) {
+                                                    e.printStackTrace();
+                                                }
+
+
                                             }
-                                            final byte[] bytes2 = bytes;
-                                            Log.d("HELLO", "KJLDJJLADS" + bytes);
-
-                                        } catch (IOException e) {
-                                            e.printStackTrace();
+                                            Log.d("HELLO", "KJLDJJLADS sssssss");
+                                            runOnUiThread(new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                    Toast.makeText(MainActivity.this, "Connection Ended",
+                                                            Toast.LENGTH_LONG).show();
+                                                }
+                                            });
                                         }
-
-
-                                    }
-                                    Log.d("HELLO", "KJLDJJLADS sssssss");
-                                    runOnUiThread(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            Toast.makeText(MainActivity.this, "Connection Ended",
-                                                    Toast.LENGTH_LONG).show();
-                                        }
-                                    });
+                                    };
+                                    thread.start();
                                 }
-                            };
-                            thread.start();
+                            })
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
+                }else{
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this, "No devices paired...",
+                                    Toast.LENGTH_LONG).show();
                         }
-                    })
-                    .setIcon(android.R.drawable.ic_dialog_alert)
-                    .show();
-        }else{
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    Toast.makeText(MainActivity.this, "No devices paired...",
-                            Toast.LENGTH_LONG).show();
+                    });
+                    return;
                 }
-            });
-            return;
+            }
+        });
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        try {
+            if(in!=null) in.close();
+            if(out!=null) out.close();
+            if(bluetoothsocket!=null) bluetoothsocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
